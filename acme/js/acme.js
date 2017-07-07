b921528a9b2705f7b909008bdccc5382077b3e38
ihc85 <cun13001@byui.edu>
2017-07-07 09:41:32 -0600

    Worked on nav bar
    Tried getting the data from JSON to get into nav bar

--- acme/js/acme.js
+++ acme/js/acme.js
@@ -1,16 +1,20 @@
 $(window).on("load", function() {
-	var ul = document.getElementsByTagName("ul");
-	console.log(ul);
-	$.ajax({
-		url: "https://ihc85.github.io/acme/js/acme.json",
-		dataType: "jsonp",
-		success: function (data) {
-			console.log(data);
-			var firstLink = data.Nav.link1;
-			console.log(firstLink);
-			document.getElementById("link1").innerHTML = link1;
-		}
+	$.getJSON("../js/acme.json", function (data) {
+		console.log(data);
+		var firstLink = data.Nav.link1;
+		console.log(firstLink);
+		document.getElementById("link1").innerHTML = link1;
 	});
+//	$.ajax({
+//		url: "../js/acme.json",
+//		dataType: "json",
+//		success: function (data) {
+//			console.log(data);
+//			var firstLink = data.Nav.link1;
+//			console.log(firstLink);
+//			document.getElementById("link1").innerHTML = link1;
+//		}
+//	});
 	var a = document.getElementById("link1");
-	a.href = "";
+	a.href = "https://ihc85.github.io/";
 });

bbf444baf57f193b5261af11c17cf47ff9ffbba6
ihc85 <cun13001@byui.edu>
2017-07-07 19:21:12 -0600

    Created original files
    New html file not working; added old css and old js files (from before
    my latest attempt to revise nav bar

---
+++ acme/js/old_acme.js
@@ -0,0 +1,32 @@
+$(window).on("load", function() {
+	$.getJSON("../js/acme.json", function (data) {
+			$("#link1").text(data.Nav.link1);
+
+	});
+	console.log();
+//	$.ajax({
+//		url: "../js/acme.json",
+//		dataType: "json",
+//		success: function (data) {
+//			console.log(data);
+//			var firstLink = data.Nav.link1;
+//			console.log(firstLink);
+//			document.getElementById("link1").innerHTML = link1;
+//		}
+//	});
+	var a = document.getElementById("link1");
+	a.href = "https://ihc85.github.io/";
+});
+
+$('#nav').on("click", "a", function(evt) {
+	var jsonLink = $(this).text();
+	console.log(jsonLink);
+	var index = $(this).index(jsonLink);
+	console.log(index);
+	getPage(index);
+});
+	function getPage(page) {
+			if (page = "Home") {
+				
+			}
+}
